a292ddecce11aee1507581d78af45a839cc984ab
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 20:17:09 +0300

    Create ai_governance.py

---
+++ project-20-enterprise-ai-platform/ai_governance.py
@@ -0,0 +1,16 @@
+def governance_check():
+
+    return {
+        "bias_check":
+        "Passed",
+
+        "compliance":
+        "Passed",
+
+        "audit_status":
+        "Approved"
+    }
+
+print(
+    governance_check()
+)
