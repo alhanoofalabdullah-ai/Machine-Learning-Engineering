d5288f5ead040ab2b221466bfeb4f8d678c58b0e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 17:42:02 +0300

    Create fraud_detection_model.py

---
+++ project-06-fraud-detection-platform/fraud_detection_model.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+import joblib
+
+df = pd.read_csv("data/transactions.csv")
+
+for col in df.select_dtypes(include="object").columns:
+    if col != "FraudStatus":
+        encoder = LabelEncoder()
+        df[col] = encoder.fit_transform(df[col])
+
+X = df.drop(["TransactionID", "FraudStatus"], axis=1)
+y = df["FraudStatus"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(
+    y_test,
+    predictions
+)
+
+print("Accuracy:", accuracy)
+print(classification_report(
+    y_test,
+    predictions
+))
+
+joblib.dump(
+    model,
+    "models/fraud_detection_model.pkl"
+)
