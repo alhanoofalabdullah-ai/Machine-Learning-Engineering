bd84ffa381f3520460a19b792a683703614ce471
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 17:38:26 +0300

    Create sales_forecasting.py

---
+++ project-05-sales-forecasting-system/sales_forecasting.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+import joblib
+
+df = pd.read_csv("data/sales_history.csv")
+
+df["Date"] = pd.to_datetime(df["Date"])
+
+df["Year"] = df["Date"].dt.year
+df["Month"] = df["Date"].dt.month
+df["Day"] = df["Date"].dt.day
+
+X = df.drop(["Date", "Revenue"], axis=1)
+y = df["Revenue"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestRegressor(
+    n_estimators=200,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, predictions)
+r2 = r2_score(y_test, predictions)
+
+print("MAE:", mae)
+print("R2 Score:", r2)
+
+joblib.dump(
+    model,
+    "models/sales_forecasting_model.pkl"
+)
