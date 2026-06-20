583782d51683304218054831a6d9940b2c95854b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 18:13:07 +0300

    Create disease_prediction_api.py

---
+++ project-12-healthcare-disease-prediction/disease_prediction_api.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load(
+    "../models/disease_prediction_model.pkl"
+)
+
+scaler = joblib.load(
+    "../models/scaler.pkl"
+)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.json
+
+    df = pd.DataFrame([data])
+
+    scaled = scaler.transform(df)
+
+    prediction = model.predict(
+        scaled
+    )[0]
+
+    return jsonify({
+        "disease_risk": int(prediction)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
